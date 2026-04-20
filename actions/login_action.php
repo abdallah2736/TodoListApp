4b8e3b5341806deabb37966795edcb265b4502aa
AbdullahFadlallah <s444002736@uqu.edu.sa>
2026-04-20 10:29:21 +0300

    Remove redundant conditional formatting and stray closing PHP tags (?>) and Move $email and $password POST assignments to the top of the file

--- actions/login_action.php
+++ actions/login_action.php
@@ -2,17 +2,17 @@
 session_start(); 
 require_once __DIR__ . '/../DB.php';
 
+$email = $_POST['email'];
+$password = $_POST['password'];
+
 if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
 
     $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
 
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
 
-        // تخزين المعلومات في الجلسة
-        if (password_verify($password, $user['password'])) {
+            if (password_verify($password, $user['password'])) {
             $_SESSION['name'] = $user['name'];
             $_SESSION['email'] = $user['email'];
             $_SESSION['id'] = $user['id']; //
@@ -29,8 +29,7 @@ if (isset($_POST['login'])) {
 
     //اذا كنت كلمه السر خطا او الحساب غير موجود
     $_SESSION['login_error'] = 'Incorrect email or password';
+    $stmt->close();
     header("Location: ../TodoListApp/Login.php");
     exit();
-    }
-?>
-<!-- سسسسسسسسسسسس -->
+}
