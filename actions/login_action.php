e0005b2d3962c07acc3f978b5f04536d6bd5e2f6
AbdullahFadlallah <s444002736@uqu.edu.sa>
2026-04-20 15:14:30 +0300

    Fixing a problem

--- actions/login_action.php
+++ actions/login_action.php
@@ -2,34 +2,31 @@
 session_start(); 
 require_once __DIR__ . '/../DB.php';
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-
 if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (empty($email) || empty($password)) {
+        $_SESSION['login_error'] = "Please fill in all fields.";
+        header("Location: ../Login.php"); 
+        exit();
+    }
 
     $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
 
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
 
-            if (password_verify($password, $user['password'])) {
+        if (password_verify($password, $user['password'])) {
             $_SESSION['name'] = $user['name'];
             $_SESSION['email'] = $user['email'];
-            $_SESSION['id'] = $user['id']; //
-            header("Location: ../TodoListApp/index.php");
+            $_SESSION['id'] = $user['id']; 
+            header("Location: ../index.php"); 
             exit();
         }
     }
-    
-    if (empty($_POST['email']) || empty($_POST['password'])) {
-        $_SESSION['login_error'] = "Please fill in all fields.";
-        header("Location: ../TodoListApp/Login.php");
-        exit();
-    }
 
-    //اذا كنت كلمه السر خطا او الحساب غير موجود
     $_SESSION['login_error'] = 'Incorrect email or password';
-    $stmt->close();
-    header("Location: ../TodoListApp/Login.php");
+    header("Location: ../Login.php");
     exit();
 }
